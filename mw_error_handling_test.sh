50b2505c750297aeae516f4ed6539de6619c9937
James <james.kizer@gmail.com>
2015-03-25 14:40:46 -0400

    aded error test

---
+++ mw_error_handling_test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .05
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .05
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .05
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .05
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .05
+
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .01
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .01
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .01
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .01
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .01
+
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .005
+
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .001
+
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0005
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0005
+
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0001
+mpirun -n 16 -hostfile ../hosts ./divisors_app_aws 500000000000000000 100000 .0001
